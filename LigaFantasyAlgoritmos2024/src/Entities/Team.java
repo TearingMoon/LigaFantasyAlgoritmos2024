72e192c0fb3d217468bc32191bdaa1bdf0a9136c
David Torrubia <davidtorrubiasantos@gmail.com>
2024-12-17 09:41:19 +0100

    Implemented starting classes and interface

---
+++ LigaFantasyAlgoritmos2024/src/Entities/Team.java
@@ -0,0 +1,16 @@
+package Entities;
+
+public class Team {
+	String name;
+	int score;
+	int goalsFor;
+	int goalsAgainst;
+
+	public Team(String name, int score, int goalsFor, int goalsAgainst) {
+		this.name = name;
+		this.score = score;
+		this.goalsFor = goalsFor;
+		this.goalsAgainst = goalsAgainst;
+	}
+	
+}
