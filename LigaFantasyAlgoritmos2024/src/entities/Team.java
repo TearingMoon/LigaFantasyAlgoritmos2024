a1da118834dd847fb217c3dc9c6509575ad29200
Ikeer7 <alu.161068@usj.es>
2024-12-17 10:40:21 +0100

    Start of addpoints method

--- LigaFantasyAlgoritmos2024/src/entities/Team.java
+++ LigaFantasyAlgoritmos2024/src/entities/Team.java
@@ -1,50 +1,18 @@
 package entities;
 
-public class Team 
-{
-	private String name;
-	private int score;
-	private int goalsFor;
-	private int goalsAgainst;
+public class Team {
+	String name;
+	int score;
+	int goalsFor;
+	int goalsAgainst;
 
-	public Team(String name, int score, int goalsFor, int goalsAgainst)
-	{
+	public Team(String name, int score, int goalsFor, int goalsAgainst) {
 		this.name = name;
 		this.score = score;
 		this.goalsFor = goalsFor;
 		this.goalsAgainst = goalsAgainst;
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-	public void setScore(int score) throws Exception {
-		if(score >= 0) this.score = score;
-		else throw new Exception("Se ha intentado poner un score negativo");
-	}
-
-	public int getGoalsFor() {
-		return goalsFor;
-	}
-
-	public void setGoalsFor(int goalsFor) {
-		this.goalsFor = goalsFor;
-	}
-
-	public int getGoalsAgainst() {
-		return goalsAgainst;
-	}
-
-	public void setGoalsAgainst(int goalsAgainst) {
-		this.goalsAgainst = goalsAgainst;
-	}
+	
+	
+	
 }
--- LigaFantasyAlgoritmos2024/src/main/FantasyLeague.java
+++ LigaFantasyAlgoritmos2024/src/main/FantasyLeague.java
@@ -1,5 +1,30 @@
 package main;
+import java.util.Scanner;
+import structures.HashTable;
+import structures.HashTable.exploration;
+import entities.Team;
+
 
 public class FantasyLeague {
 
+	int numberOfTeams;
+	Scanner sc = new Scanner(System.in);
+	HashTable<Team> table = new HashTable<Team>(0, exploration.LINEAL);
+	
+	public void addPoints(String identifier) {
+		int points;
+		if(false) return; //TODO: metodo hash de búsqueda
+		else {
+			do {
+				System.out.println("Cuantos puntos ha conseguido tu equipo: ");
+				 points = sc.nextInt();
+				
+			}while(points !=0 || points !=1 || points !=3);
+			
+			
+		
+				
+			
+		}
+	}
 }
